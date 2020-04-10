8ffc060242668604914b25fd7fe4448b3d12044f
Jan Trtík <jan.trtik@bonami.cz>
2020-04-10 21:37:45 +0200

    Refactor IO program further
    Using kleisli

--- src/main/scala/hydro/Subscriber.scala
+++ src/main/scala/hydro/Subscriber.scala
@@ -1,11 +1,12 @@
 package hydro
 
+import cats.Applicative
+import cats.data.Kleisli
 import cats.implicits._
 import cats.effect._
-import doobie._
-import doobie.implicits._
-import doobie.implicits.javatime._
+import fs2.Stream
 import hydro.domain.Measurement.{ Ec, Ph, Temperature }
+import hydro.domain.{ MeasurementRepository, MeasurementSource }
 import hydro.infrastructure.{ DoobieMeasurementRepository, DoobieTransactor, MqttMeasurementSource }
 import hydro.infrastructure.MqttMeasurementSource.Topic
 import org.http4s.Uri
@@ -16,37 +17,48 @@ object Subscriber extends IOApp with LogSupport {
   Logger.setDefaultLogLevel(LogLevel.DEBUG)
   Logger.setDefaultFormatter(LogFormatter.IntelliJLogFormatter)
 
+  private val doobieConfig = DoobieTransactor.Config(
+    "org.postgresql.Driver",
+    "jdbc:postgresql://127.0.0.1/hydro",
+    "hydro",
+    "hydr0hydrO",
+  )
+
+  private val mqttConfig = MqttMeasurementSource.Config(
+    Uri.unsafeFromString("tcp://test.mosquitto.org"),
+    Topic("hydro/1/+"),
+    Map(
+      Topic("hydro/1/temperature") -> Temperature,
+      Topic("hydro/1/ec") -> Ec,
+      Topic("hydro/1/ph") -> Ph,
+    )
+  )
+
+  trait Context[F[_]] {
+    def measurementRepository: MeasurementRepository[F]
+    def measurementSource: MeasurementSource[F]
+  }
+
+  private def program[F[_]: Applicative](implicit compiler: Stream.Compiler[F, F]):  Kleisli[F, Context[F], ExitCode] = {
+    Kleisli[F, Context[F], ExitCode] { context =>
+      context.measurementSource
+        .makeStream
+        .evalMapChunk(context.measurementRepository.save)
+        .compile
+        .drain
+        .as(ExitCode.Success)
+    }
+  }
+
   def run(args: List[String]): IO[ExitCode] = {
-
-    val doobieConfig = DoobieTransactor.Config(
-      "org.postgresql.Driver",
-      "jdbc:postgresql://127.0.0.1/hydro",
-      "hydro",
-      "hydr0hydrO",
-    )
-
-    val mqttConfig = MqttMeasurementSource.Config(
-      Uri.unsafeFromString("tcp://test.mosquitto.org"),
-      Topic("hydro/1/+"),
-      Map(
-        Topic("hydro/1/temperature") -> Temperature,
-        Topic("hydro/1/ec") -> Ec,
-        Topic("hydro/1/ph") -> Ph,
-      )
-    )
-
     val clock = Clock.create[IO]
     val transactor = DoobieTransactor
       .make(doobieConfig)
 
-    val repository = new DoobieMeasurementRepository(transactor, clock)
-
-    MqttMeasurementSource(mqttConfig)
-      .makeStream
-      .evalMapChunk(repository.save)
-      .compile
-      .drain
-      .as(ExitCode.Success)
+    program[IO].run(new Context[IO] {
+      def measurementRepository: MeasurementRepository[IO] = new DoobieMeasurementRepository(transactor, clock)
+      def measurementSource: MeasurementSource[IO] = MqttMeasurementSource(mqttConfig)
+    })
   }
 
 }
